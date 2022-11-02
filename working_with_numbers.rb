e278b13f212725dbb3223ed2faf93cc9d0caaf28
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-11-02 13:16:41 +0000

    practice with numbers, strings and created a simple calculator

---
+++ working_with_numbers.rb
@@ -0,0 +1,76 @@
+#NUMBERS
+
+# puts 1 + 2
+
+# 10.0 / 4  #to float or 10 / 4.to_f
+
+# x = 5
+
+# y = 10
+
+# puts y / x
+
+
+# irb(main):002:0> 10 / 4
+# => 2
+# irb(main):003:0> 10.0 / 4
+# => 2.5
+# irb(main):004:0> "-" * 20
+# => "--------------------"
+# irb(main):005:0> 
+
+# irb(main):005:0> "5" * 2
+# => "55"
+# irb(main):006:0> 2 * "5" Error
+
+
+# 20.times {print "-"}
+
+
+# irb(main):008:0> 20.times { puts ra
+# nd(10)}
+# 5
+# 4                                  
+# 3                                  
+# 3                                  
+# 9                                  
+# 8                                  
+# 8                                  
+# 6                                  
+# 6                                  
+# 6                                  
+# 2                                  
+# 5                                  
+# 3                                  
+# 1                                  
+# 0
+# 6
+# 4
+# 7
+# 3
+# 2
+# => 20
+
+# irb(main):009:0> rand (100)
+# => 31
+# irb(main):010:0> 
+
+# irb(main):012:0> x.to_f
+# => 5.0
+# irb(main):013:0> y = "10".to_f 
+# => 10.0
+# irb(main):014:0> x * y
+# => 50.0
+# irb(main):015:0> 
+
+
+
+puts "Simple calculator"
+
+25.times { print "-" }
+puts
+puts "Enter the first number"
+num_1 = gets.chomp
+puts "Enter the second number"
+num_2 = gets.chomp
+puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
