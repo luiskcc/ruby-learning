d2d531bc7fc546799fc1cd367c1f6a699b481e38
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-11-02 12:37:17 +0000

    first commit

---
+++ ruby_playground.rb
@@ -0,0 +1,19 @@
+#------------DATA TYPES---------------
+
+# puts "Hello World"
+# puts "Hello World"
+# p "Hello"
+# print "Hello World\n" #Skips to the next line
+# print " Hello World"
+
+
+greeting = "Hello World"
+
+puts greeting
+
+
+def say_hello(thing_to_say)
+    puts thing_to_say
+end
+
+say_hello "Hello world is great!"
---
+++ strings.rb
@@ -0,0 +1,31 @@
+
+# # String concatenation
+# sentence= "Myname is Mashur" #with single strings wont be able to use insterpolation
+# p sentence
+
+# first_name= "Luis"
+# last_name = "Kerch"
+# puts first_name + " " + last_name
+
+# #String insterpolation
+# puts "My first name is #{first_name} and my last name is #{last_name}"
+
+# #Custom methods
+# #.irb on terminal .methods to see the methods
+# # ESCAPIGN SPECIAL CHARACTERS 'hey john \'Hello\''
+# # => "hey john 'Hello'"
+
+
+puts "What is your first name?"
+
+# first_name = gets.chomp #to get input from the command line and assign it
+
+# puts "Thank you you said your first name is #{first_name}"
+
+
+# puts "Enter a number to multiply by 2"
+
+# input  = gets.chomp
+# puts input.to_i * 2
+
+ 
