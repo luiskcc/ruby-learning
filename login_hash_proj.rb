787c26d1c54184ebbf73ad4ca0705ef8341c913f
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-11-10 18:57:07 +0000

    loop of login and username

--- login_hash_proj.rb
+++ login_hash_proj.rb
@@ -16,29 +16,76 @@ puts "Welcome to the authenticator"
 25.times { print "-"}
 puts
 puts "THIS PROGRAM WILL TAKE AN INPUT FROM A USER AND COMPARE"
-puts "I password is correct, you will hget back the user object"
+puts "If password is correct, you will hget back the user object"
 
-# users.each do [str, value]
-#     if 
-#     puts = value.string
-#     puts "the password is correct for #{value.string}"
-#     else
-#         puts "password incorrect"
-#     end
 
-# end
+
 
 attempts = 1
 
+puts "ABORT BY PRESSING 'N'"
+
 while attempts < 4 #while true is going to keep looping until you end it
-puts "this will keep printing"
-attempts += 1
+print "Username: "
+username = gets.chomp
+
+
+if username == "n"
+    break
+end
+
+
+users.each do |user|
+    if user [:username] == username
+        puts "your username is correct, thank you #{username}"
+        break
+    else
+        puts "try another username"
+        break
+    end
+
 end
 
 
 
-x = (1..100)
+    print "Password: "
+    password = gets.chomp
 
-while true do
-    puts x.to_a
+    users.each do |user|
+        if user [:password] == password
+        
+        
+
+            puts "Password was correct #{password}"
+            break
+
+        
+
+    else
+        puts "Try another password"
+        break
+
+
+    end
+
+    attempts += 1
+
 end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+# x = (1..100)
+
+# while true do
+#     puts x.to_a
+# end
