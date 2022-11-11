09c16a2e47cb0530f079373c9fdca6961af5ff61
luiskcc <114909465+luiskcc@users.noreply.github.com>
2022-11-11 08:40:05 +0000

    Login real proj (#13)

--- login_hash_proj.rb
+++ login_hash_proj.rb
@@ -16,29 +16,4 @@ puts "Welcome to the authenticator"
 25.times { print "-"}
 puts
 puts "THIS PROGRAM WILL TAKE AN INPUT FROM A USER AND COMPARE"
-puts "I password is correct, you will hget back the user object"
 
-# users.each do [str, value]
-#     if 
-#     puts = value.string
-#     puts "the password is correct for #{value.string}"
-#     else
-#         puts "password incorrect"
-#     end
-
-# end
-
-attempts = 1
-
-while attempts < 4 #while true is going to keep looping until you end it
-puts "this will keep printing"
-attempts += 1
-end
-
-
-
-x = (1..100)
-
-while true do
-    puts x.to_a
-end
