6f06e7b031f22eeafe8ad002d3bf2ba01dcd07f3
luiskcc <114909465+luiskcc@users.noreply.github.com>
2022-11-11 13:34:36 +0000

    set up of project (#16)

---
+++ area_code_dic_proj.rb
@@ -0,0 +1,32 @@
+# ------------ 35 . DICTIONARY PROJECT ------------ 
+
+
+dial_book = {
+
+    "newyork" => "212"
+    "newbrunswick" => "732",
+    "edison" => "908",
+    "plainsboro" => "609",
+    "sanfrancisco" => "301",
+    "miami" => "305",
+    "paloalto" => "650",
+    "evanston" => "847",
+    "orlando" => "407",
+    "lancaster" => "717"
+}
+
+
+#Get city names from the hash
+def get_city_names (somehash)
+
+end
+
+#Get area code based on given hash and key
+def get_area_code(somehash)
+end
+
+
+
+loop do
+
+end
