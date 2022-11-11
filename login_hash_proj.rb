94c4ce29fd0e2bc8695197f5c8eb834270609a40
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2022-11-11 08:42:13 +0000

    looping through username and passsword in users

--- login_hash_proj.rb
+++ login_hash_proj.rb
@@ -17,3 +17,79 @@ puts "Welcome to the authenticator"
 puts
 puts "THIS PROGRAM WILL TAKE AN INPUT FROM A USER AND COMPARE"
 
+#This is a Hash Login project#
+
+users = [
+    { username: "luis", password: "1" },
+    { username: "fede", password: "2" },
+    { username: "lito", password: "3" },
+    { username: "javi", password: "4" },
+    { username: "leon", password: "5" },
+
+]
+
+
+puts users
+
+puts "Welcome to the authenticator"
+25.times { print "-"}
+puts
+puts "THIS PROGRAM WILL TAKE AN INPUT FROM A USER AND COMPARE"
+puts "If password is correct, you will hget back the user object"
+
+
+
+
+attempts = 1
+
+puts "ABORT BY PRESSING 'N'"
+
+while attempts < 4 #while true is going to keep looping until you end it
+print "Username: "
+username = gets.chomp
+
+
+if username == "n"
+    break
+end
+
+
+users.each do |user|
+    if user [:username] == username
+        puts "your username is correct, thank you #{username}"
+        break
+    else
+        puts "try another username"
+        break
+    end
+
+end
+
+
+
+    print "Password: "
+    password = gets.chomp
+
+    users.each do |user|
+        if user [:password] == password
+        
+        
+
+            puts "Password was correct #{password}"
+            break
+
+        
+
+    else
+        puts "Try another password"
+        break
+
+
+    end
+
+    attempts += 1
+
+end
+
+end
+
