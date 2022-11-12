d03b9f39e78391d1509db4e44824a55fc66e1781
luiskcc <114909465+luiskcc@users.noreply.github.com>
2022-11-12 14:39:45 +0000

    fixed error and added loop (#17)

--- area_code_dic_proj.rb
+++ area_code_dic_proj.rb
@@ -3,7 +3,7 @@
 
 dial_book = {
 
-    "newyork" => "212"
+    "newyork" => "212",
     "newbrunswick" => "732",
     "edison" => "908",
     "plainsboro" => "609",
@@ -28,5 +28,11 @@ end
 
 
 loop do
+    puts "Do you want to lookup an area code based on a city name (Y/N)"
+    answer = gets.chomp.downcase 
+    break if answer != "y"
+    
+
+
 
 end
