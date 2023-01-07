a04af30208a9fdfc39bcf373a88fdc6ae5fc01d7
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2023-01-07 16:32:36 +0000

    area code proj

--- area_code_dic_proj.rb
+++ area_code_dic_proj.rb
@@ -18,11 +18,13 @@ dial_book = {
 
 #Get city names from the hash
 def get_city_names (somehash)
-
+    somehash.keys
 end
 
 #Get area code based on given hash and key
-def get_area_code(somehash)
+def get_area_code(somehash, key)
+    somehash[key]
+
 end
 
 
@@ -31,8 +33,22 @@ loop do
     puts "Do you want to lookup an area code based on a city name (Y/N)"
     answer = gets.chomp.downcase 
     break if answer != "y"
+    puts "which city do you want to look the area code for?"
+    puts get_city_names(dial_book)
+    puts "Enter city from the list"
+    loop do
+    prompt = gets.chomp
+    if dial_book.include?(prompt) 
+        puts "Your area code for #{prompt} is #{get_area_code(dial_book, prompt)}"
+        puts "Do you want to look for any other city code? (Y/N)"
+        answer = gets.chomp.downcase 
+        break if answer != "y"
+        puts "Enter city from the list"
     
-
-
+    else
+    puts "you entered an invalid city name try again"
+    
+    end
+    end
 
 end
