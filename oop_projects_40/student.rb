d5f1f98fe6c23937bbd2f239dd840db84187a6bb
luiskcc <114909465+luiskcc@users.noreply.github.com>
2023-01-12 18:32:23 +0000

    Include crud (#21)
    * module added + self. or Crud.
    
    * include Crud for using modules inside classes

--- oop_projects_40/student.rb
+++ oop_projects_40/student.rb
@@ -1,11 +1,15 @@
 class Student
+attr_accesor :first_name, :last_name, :email, :username, :password
 
-@first_name
-@last_name
-@age
-@email
-@username
-@password
+
+def initialize(firsname, lastname, username, email, password)
+    @first_name
+    @last_name
+    @age
+    @email
+    @username
+    @password
+end
 
 def to_st
     "First Name #{@first_name}"
